4af5ded689693253f5c9eb805f01a8ce5fd65018
andrewbihl <andrewbihlva@gmail.com>
2016-11-19 17:07:00 -0500

    What up

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    if "penis" or "hate" in message_text:
+                    if "mean" in message_text or "hate" in message_text:
                         send_delayed_message(sender_id, "You're ugly.", 10.0)
                         continue
                     send_message(sender_id, "got it, thanks!")
