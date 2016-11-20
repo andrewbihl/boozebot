131f23b5520bb85d5db3acaad7e20230dd9433b1
andrewbihl <andrewbihlva@gmail.com>
2016-11-20 00:37:22 -0500

    Add line to remove existing jobs

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    send_delayed_message(sender_id, "<Generic response>", 5)
+                    send_delayed_message(sender_id, "Eyooo", 5)
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
 
@@ -55,11 +55,12 @@ def webhook():
 
 def send_delayed_message(recipient_id, message_text, time_in_seconds):
     scheduler = BackgroundScheduler()
+    scheduler.remove_all_jobs()
     print(scheduler.get_jobs())
     current_date = datetime.datetime.now()
     print(time_in_seconds)
     send_time = datetime.datetime(current_date.year, current_date.month, current_date.day, current_date.hour, current_date.minute, current_date.second+time_in_seconds)
-    scheduler.add_job(send_message(recipient_id, message_text),trigger='date', run_date=send_time)
+    scheduler.add_job(send_message(recipient_id, message_text), trigger='date', run_date=send_time)
     scheduler.start()
 
 def send_message(recipient_id, message_text):
