6a53a33e11c7672a28405a3b5591d7203a566cfe
andrewbihl <andrewbihlva@gmail.com>
2016-11-20 01:29:10 -0500

    asd

--- app.py
+++ app.py
@@ -40,8 +40,6 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    scheduler = BackgroundScheduler()
-                    scheduler.remove_all_jobs()
                     send_message(sender_id, "k")
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
